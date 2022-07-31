99bdfd5f97f166883799b86ebbc91475cb468618
Sborresch <109110034+Sborresch@users.noreply.github.com>
2022-07-30 19:25:21 -0600

    Add files via upload

---
+++ election_analysis/Python_practice.py
@@ -0,0 +1,10 @@
+counties_dict = {"Arapahoe":422829, "Denver":463353, "Jefferson":432438}
+
+for county in counties_dict:
+    print(county)
+
+for registered_voters in counties_dict:
+    print(registered_voters)
+
+for county in counties_dict.values:
+    print (county)
